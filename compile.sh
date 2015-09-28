73c1dbade2e5bbecbe426fadf453f3017b06c651
Koen Wolters <koen@bluejayeindhoven.nl>
2015-09-28 11:59:42 +0200

    remove system dependant files from version control

--- compile.sh
+++
@@ -1,3 +0,0 @@
-cd ../../
-source sitlinit
-catkin_make_isolated -DPYTHON_EXECUTABLE=/usr/bin/python2 -DPYTHON_INCLUDE_DIR=/usr/include/python2.7/ --pkg active_safety
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source ../../devel_isolated/setup.bash
-rosrun active_safety active_safety 
